8ad3ae36ee37ce5de36a517dd485c9b6741fcd24
marwa <marwa.zaabi2012@gmail.com>
2021-05-06 09:54:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import  Button from './Component/Button'
+import  Form from './Component/Form'
+function App() {
+  return (
+  <div>
+  <Button/>
+  <Form/>
+  </div>
+  )
+}
+
+export default App;
